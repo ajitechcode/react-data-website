faa3834536b3addf1eaea36cca80649c887e9dad
ajitechcode <vrexvice@gmail.com>
2022-12-29 09:22:37 +0100

    first commit

---
+++ src/data/CarouselData.js
@@ -0,0 +1,57 @@
+export const data = [
+  {
+    title: "What our clients say",
+    description:
+      "Our clients happily stay with our services for more several years now. See real reviews from our clients.",
+    image:
+      "https://th.bing.com/th/id/R.61b54b8bfae572abb4043be9d2ae5bdb?rik=i%2b02fJXoUCEdNA&pid=ImgRaw&r=0"
+  },
+  {
+    title: "Our security ",
+    description:
+      "Learn more about our security systems to make sure your data is always safe",
+    image:
+      "https://venturebeat.com/wp-content/uploads/2015/01/security.jpg?w=780&strip=all"
+  },
+  {
+    title: "Our Team",
+    description:
+      "Our team consists of the best experts in the industry, learn about them",
+    image:
+      "https://th.bing.com/th/id/R.1a8dca04325abefe5e8ea810435dffae?rik=rDZCKI14IdLsNQ&pid=ImgRaw&r=0"
+  },
+  {
+    title: "Our servers",
+    description: "Find more about hardware and software used for your service",
+    image:
+      "https://www.theravensociety.com/wp-content/uploads/2020/02/1f-2048x1434.jpg"
+  },
+  {
+    title: "Our top clients",
+    description: "We have provided services to top clients in tech industry",
+    image:
+      "https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+  }
+];
+
+export const sliderSettings = {
+  arrows: false,
+  slidesToShow: 3,
+  focusOnselect: false,
+  accessability: false,
+  responsive: [
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+};
---
+++ src/data/HeroData.js
@@ -0,0 +1,49 @@
+export const heroOne = {
+  reverse: true,
+  inverse: true,
+  topLine: {
+    text: "Founded in 2022"
+  },
+  headline: "We've been in business for 3 years",
+  description:
+    "We have designed and implemented the best cloud security solutions",
+  buttonLabel: "Find More",
+  imgStart: "start",
+  img: "https://wallpaperaccess.com/full/2675404.jpg",
+  start: "true"
+};
+
+export const heroTwo = {
+  reverse: false,
+  inverse: false,
+  topLine: {
+    text: "Designed & Developed"
+  },
+  headline: "The best practices",
+  description: "Our clients have had the best experience working with us",
+  buttonLabel: "View Project",
+
+  linkTo: "/more",
+  imgStart: "start",
+  img:
+    "https://th.bing.com/th/id/R.ab5b0d230fdd9f3a98795f9232827552?rik=deEe3PZYSDHFcw&pid=ImgRaw&r=0",
+  start: "true"
+};
+
+export const heroThree = {
+  reverse: true,
+  inverse: true,
+  topLine: {
+    text: "Highly reputed brand"
+  },
+  headline: "Why us? ",
+  description:
+    "Our 3 year experience have allowed us to use the most innovative technologies and methodologies",
+  buttonLabel: "View Project",
+
+  linkTo: "/download",
+  imgStart: "",
+  img:
+    "https://th.bing.com/th/id/OIP.ciDfqAyf_hKwN_OqQLwkAAHaE8?pid=ImgDet&rs=1",
+  start: "true"
+};
